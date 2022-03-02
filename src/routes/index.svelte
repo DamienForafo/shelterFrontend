a07e02ba4e08699594c022e7ae78e9e814d58683
DamienForafo <damien.forafo@gmail.com>
2022-03-02 16:03:02 +0100

    opacity effect

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,34 +1,36 @@
 <script>
-    let oldValue = 0
-    let newValue = 0
-    let topBarClasses = 'coucou';
-    function hideTopBar(e) {
-        console.log('scroll');
-        oldValue = newValue;
-        newValue = window.pageYOffset;
-        if (oldValue < newValue) {
-            topBarClasses = 'hidden';
-        }
-        else if (oldValue > newValue) {
-            topBarClasses = '';
-        }
+    import {onMount} from 'svelte';
+    let oldYOffset = 0;
+    let newYOffset = 0;
+    let topBarClasses = '';
+    let topBar;
+    let topBarOffsetTop = 0;
+    onMount(() => {
+        topBarOffsetTop = topBar.offsetTop;
+    });
+    function manageScroll(e) {
+        document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight)); // % of the page
+        document.body.style.setProperty('--topScroll', (window.pageYOffset - topBarOffsetTop) / topBarOffsetTop); // % of the height between topBar & cards
+        oldYOffset = newYOffset;
+        newYOffset = window.pageYOffset;
+        if (oldYOffset < newYOffset) topBarClasses = '';
+        else topBarClasses = 'show';
     }
 </script>
 
 
+<svelte:window on:scroll={manageScroll}/>
 
-<svelte:window on:scroll={hideTopBar}/>
-
-
-<div id ="topBar" class={topBarClasses}>
-    <button id="menuButton">
-
-    </button>
+<div bind:this={topBar} id ="topBar" class={topBarClasses}>
+    <div id="topBarBackground"></div>
     <input id="searchInput" type="text" name="searchInput">
+    <div id="tags">
+
+    </div>
 </div>
 
 
-<div id="bidon"> <!--à virer-->
+<div id="cards"> <!--à virer-->
     <div class="carte"></div>
     <div class="carte"></div>
     <div class="carte"></div>
@@ -51,53 +53,66 @@
     <div class="carte"></div>
 </div>
 
-
-
-
-<style lang="less">
-    :root {
-        --topBarHeight: 50px;
+<style global lang="less">
+    body {
+        margin: 0;
+        --topBarHeight: 120px;
+        --cardsMargin: 90vh;
+        --semiMargin: calc(~"(var(--cardsMargin) - var(--topBarHeight)) / 2");
     }
 
-    #bidon {
+    #cards {
+        width: 100%;
         display: flex;
         justify-content: center;
         align-items: center;
         flex-wrap: wrap;
-        margin-top: 200px;
+        margin-top: var(--semiMargin);
     }
 
     .carte {
         width: 300px;
         height: 300px;
-        background: darkcyan;
+        background: #c8d8f0;
         margin: 30px;
     }
 
     #topBar {
-        position: fixed;
-        top: 0;
+        position: -webkit-sticky;
+        position: sticky;
+        top: calc(~"0px - var(--topBarHeight) - 1px");
+        margin-top: var(--semiMargin);
         left: 0;
-        background: seagreen;
         width : 100%;
         height: var(--topBarHeight);
         display: flex;
+        flex-direction: column;
         justify-content: center;
         align-items: center;
         transition: top 0.15s;
-        &.hidden {
-            top: calc(0px - var(--topBarHeight) - 1px);
+        z-index: 1;
+        > #topBarBackground {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            top: 0;
+            left: 0;
+            background: #b886bb;
+            opacity: var(--topScroll);
         }
-    }
-    #topBar.hidden {
-         top: calc(0px - var(--topBarHeight) - 1px);
-    }
-
-    #menuButton {
-        --menuButtonWidth: 30px;
-        width: var(--menuButtonWidth);
-        height: var(--menuButtonWidth);
-        position: absolute;
-        left: calc((var(--topBarHeight) - var(--menuButtonWidth)) / 2);
+        &.show {
+            top: -1px;
+        }
+        input {
+            height: 40px;
+            margin-bottom: 10px;
+            z-index: 1;
+        }
+        #tags {
+            width: 200px;
+            height: 40px;
+            background: blueviolet;
+            z-index: 1;
+        }
     }
 </style>
